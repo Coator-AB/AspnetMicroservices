7f81d7b911bdca47ec1e5d31f01367bd4b9268bd
Linus Lindström <linus.lindstrom@coator.se>
2022-08-30 01:43:13 +0200

    Added added ocelot

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,23 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true); 
+
+// Configure logging.
+ builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+// Use Ocelot
+await app.UseOcelot();
+
+app.Run();

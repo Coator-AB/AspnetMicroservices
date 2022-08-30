0874959803a18c9aebbf5c0511003b1a86d94d17
Linus Lindström <linus.lindstrom@coator.se>
2022-08-30 15:29:22 +0200

    Finished Shopping.Aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping.Aggregator.Models;
+
+public class ShoppingModel
+{
+    public string Username { get; set; }
+    public BasketModel BasketWithProducts { get; set; }
+    public IEnumerable<OrderResponseModel> Orders { get; set; }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly HttpClient _client;
+
+    public OrderService(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUsername(string username)
+    {
+        var response = await _client.GetAsync($"/api/v1/Order/{username}");
+        return await response.ReadContentAs<List<OrderResponseModel>>();
+    }
+}
